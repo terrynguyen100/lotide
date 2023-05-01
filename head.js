95802c6d79cee7285fa8fc78f34f9bd53484df84
Terry Nguyen <terrynguyen100@gmail.com>
2023-05-01 17:24:41 -0600

    eslinted

--- head.js
+++ head.js
@@ -10,10 +10,10 @@ const assertEqual = function(actual, expected) {
 
 const head = function(array) {
   return array[0];
-}
+};
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head([11,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 1)
-assertEqual(head([]), 1)
+assertEqual(head([1]), 1);
+assertEqual(head([]), 1);
