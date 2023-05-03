5544617cf2780638301d32931b169f2773b00483
Terry Nguyen <terrynguyen100@gmail.com>
2023-05-03 15:54:26 -0600

    added letterPosition.js

---
+++ letterPositions.js
@@ -0,0 +1,39 @@
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length)
+    return false;
+  for (let i = 0; i <= arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  
+  let result = '';
+  if (eqArrays(actual, expected)) {
+    result = `\u{2705} Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    result = `\u{274c} Assertion Failed: ${actual} !== ${expected}`;
+  }
+  console.log(result);
+};
+
+const letterPositions = function(sentence) {
+  const result = {};
+  
+  for (let i = 0; i < sentence.length; i++) {
+    if (result[sentence[i]]) {
+      result[sentence[i]].push(i);
+    } else {
+      result[sentence[i]] = [i];
+    }
+  }
+
+  return result;
+};
+
+
+console.log(letterPositions('aaabbbccc'));
