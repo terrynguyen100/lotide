44b6a1d43df010270a470030d8cd3b4a607577de
Terry Nguyen <terrynguyen100@gmail.com>
2023-05-03 15:41:35 -0600

    add countonly.js

---
+++ countOnly.js
@@ -0,0 +1,45 @@
+const assertEqual = function(actual, expected) {
+  let result = '';
+  if (actual === expected) {
+    result = `\u{2705} Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    result = `\u{274c} Assertion Failed: ${actual} !== ${expected}`;
+  }
+  console.log(result);
+};
+
+const countOnly = function (allItems, itemsToCount) {
+  let result = {};
+
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (result[item]) {
+        result[item] += 1;
+      } else {
+        result[item] = 1;
+      }
+    }
+  }
+  return result;
+}
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
+
