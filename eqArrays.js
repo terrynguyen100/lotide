ff4f3f8d7bb6c41cebfcd5d358d43d1cd88f459c
Terry Nguyen <terrynguyen100@gmail.com>
2023-05-04 15:57:13 -0600

    update

--- eqArrays.js
+++ eqArrays.js
@@ -11,8 +11,10 @@ const eqArrays = function(arrayOne, arrayTwo) {
 };
 
 console.log(eqArrays([1, 2, 3], [1, 2, 3]))
+//true
 
 console.log(eqArrays([1, 2, 3], [3, 2, 1]))
+//false
 
 console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]))
  // => true
