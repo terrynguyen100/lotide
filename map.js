5dd325fb53065a082864942b89472c4a39d46805
Terry Nguyen <terrynguyen100@gmail.com>
2023-05-04 15:56:34 -0600

    add map.js

---
+++ map.js
@@ -0,0 +1,36 @@
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length)
+    return false;
+  for (let i = 0; i <= arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  
+  let result = '';
+  if (eqArrays(actual, expected)) {
+    result = `\u{2705} Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    result = `\u{274c} Assertion Failed: ${actual} !== ${expected}`;
+  }
+  console.log(result);
+};
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const results1 = map(words, word => word[0]);
+
+assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
