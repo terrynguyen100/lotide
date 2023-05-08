dd0c9ca3398bb05649ea725369d302c3e8cfd437
Terry Nguyen <terrynguyen100@gmail.com>
2023-05-08 16:49:48 -0600

    refactor tail.js

--- tail.js
+++ tail.js
@@ -1,19 +1,6 @@
-const assertEqual = function(actual, expected) {
-  let result = '';
-  if (actual === expected) {
-    result = `\u{2705} Assertion Passed: ${actual} === ${expected}`;
-  } else {
-    result = `\u{274c} Assertion Failed: ${actual} !== ${expected}`;
-  }
-  console.log(result);
-};
-
 const tail = function(myArray) {
   const tailArray = myArray.slice(1);
   return tailArray;
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+module.exports = tail;
---
+++ test/tailTest.js
@@ -0,0 +1,8 @@
+const tail = require('../tail');
+const assertEqual = require('../assertEqual');
+
+// test code
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
