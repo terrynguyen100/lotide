b8ccc627bcee0dc66e653a098929c5a837b7e3c2
Terry Nguyen <terrynguyen100@gmail.com>
2023-05-08 16:58:13 -0600

    refactor middle

--- middle.js
+++ middle.js
@@ -37,9 +37,5 @@ const middle = function(myArray) {
   result.push(myArray[myArray.length / 2]);
   return result;
 };
-console.log(middle([1]));
-console.log(middle([1, 2]));
-console.log(middle([1, 2, 3]));
-console.log(middle([1, 2, 3, 4, 5]));
-console.log(middle([1, 2, 3, 4]));
-console.log(middle([1, 2, 3, 4, 5, 6]));
+
+module.exports = middle;
---
+++ test/middleTest.js
@@ -0,0 +1,9 @@
+const middle = require('../middle');
+
+//test code
+console.log(middle([1]));
+console.log(middle([1, 2]));
+console.log(middle([1, 2, 3]));
+console.log(middle([1, 2, 3, 4, 5]));
+console.log(middle([1, 2, 3, 4]));
+console.log(middle([1, 2, 3, 4, 5, 6]));
